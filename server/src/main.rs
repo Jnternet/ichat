33b56393c142375552596f186e7ef2704cc9f2b3
BO <@>
2026-03-27 18:39:31 +0800

    rustls

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+use rkyv::Archived;
+use rustls::pki_types::pem::PemObject;
+use rustls::pki_types::{CertificateDer, PrivateKeyDer};
+use shared::*;
+use std::sync::Arc;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio_rustls::{TlsAcceptor, TlsStream};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+
+    let server_addr = std::env::var("SERVER_SOCK_ADDR")?;
+    let listener = TcpListener::bind(server_addr).await?;
+
+    let tls_acceptor = get_acceptor().await?;
+
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        eprintln!("客户端连接: {}", addr);
+        let tls_stream = tls_acceptor.accept(stream).await?;
+        let tls_stream = TlsStream::from(tls_stream);
+
+        tokio::spawn(async move {
+            handle_client(tls_stream).await.unwrap();
+        });
+    }
+}
+
+pub async fn get_acceptor() -> anyhow::Result<TlsAcceptor> {
+    let cert_path = std::env::var("CERT_PATH")?;
+    let key_path = std::env::var("KEY_PATH")?;
+    let certs = CertificateDer::pem_file_iter(cert_path)?
+        .map(|cert| cert.unwrap())
+        .collect::<Vec<_>>();
+    let key = PrivateKeyDer::from_pem_file(key_path)?;
+
+    let server_config = rustls::ServerConfig::builder()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)?;
+    anyhow::Ok(TlsAcceptor::from(Arc::new(server_config)))
+}
+
+pub async fn handle_client(tls_stream: TlsStream<tokio::net::TcpStream>) -> anyhow::Result<()> {
+    let (mut rh, mut wh) = tokio::io::split(tls_stream);
+
+    let mut buf = bytes::BytesMut::new();
+    rh.read_buf(&mut buf).await?;
+    let ar_test = rkyv::access::<Archived<Test>, rkyv::rancor::Error>(&buf)?;
+    dbg!(&ar_test);
+
+    wh.write_all("server respond".as_bytes()).await?;
+    wh.flush().await?;
+
+    anyhow::Ok(())
 }
---
+++ shared/src/lib.rs
@@ -0,0 +1,23 @@
+use rkyv::{Archive, Deserialize, Serialize};
+
+#[derive(Archive, Deserialize, Serialize, Debug, PartialEq)]
+#[rkyv(
+    // This will generate a PartialEq impl between our unarchived
+    // and archived types
+    compare(PartialEq),
+    // Derives can be passed through to the generated type:
+    derive(Debug),
+)]
+pub struct Test {
+    string: String,
+    option: Option<Vec<i32>>,
+}
+
+impl Test {
+    pub fn new(string: String) -> Test {
+        Test {
+            string,
+            option: None,
+        }
+    }
+}
