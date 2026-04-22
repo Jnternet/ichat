5673001c7834667bf015d13b021da1f9798931e5
BO <@>
2026-04-22 12:10:27 +0800

    tmp: chat_util

--- client/src/ui/chat/chat_util.rs
+++ client/src/ui/chat/chat_util.rs
@@ -1,20 +1,90 @@
-use std::collections::VecDeque;
-
-use sea_orm::{ConnectionTrait, DatabaseConnection};
+use crate::entity::{account_group, groups, messages};
+use sea_orm::prelude::DateTimeUtc;
+use sea_orm::{
+    ColumnTrait, ConnectionTrait, DatabaseConnection, EntityTrait, QueryFilter, QueryOrder,
+    TransactionTrait,
+};
+use shared::auth::Auth;
 use shared::group::GroupId;
+use std::collections::VecDeque;
 
 pub(super) struct OneGroup {
     id: GroupId,
     name: String,
     last_msg: Option<String>,
+    last_msg_time: Option<DateTimeUtc>,
 }
 pub(super) struct UIGroups {
     groups: VecDeque<OneGroup>,
 }
 
-pub(super) async fn get_groups_info(db: DatabaseConnection) -> UIGroups {
-    todo!()
+pub(super) async fn get_groups_info(
+    auth: Auth,
+    db: DatabaseConnection,
+) -> Result<UIGroups, String> {
+    let uid = auth.account_id();
+    let txn = db
+        .begin()
+        .await
+        .map_err(|e| format!("Failed to begin transaction: {}", e))?;
+
+    let group_records = account_group::Entity::find()
+        .filter(account_group::Column::AccountUuid.eq(uid))
+        .all(&txn)
+        .await
+        .map_err(|e| format!("Failed to query account_group: {}", e))?;
+
+    let mut groups_info: Vec<OneGroup> = Vec::new();
+    for account_group_record in group_records {
+        let group_id = GroupId(account_group_record.group_uuid);
+        let one_group = get_one_group(&txn, group_id)
+            .await
+            .map_err(|e| format!("Failed to get group info: {}", e))?;
+        groups_info.push(one_group);
+    }
+
+    groups_info.sort_by(
+        |a, b| match (a.last_msg_time.as_ref(), b.last_msg_time.as_ref()) {
+            (Some(a_time), Some(b_time)) => b_time.cmp(a_time),
+            (Some(_), None) => std::cmp::Ordering::Less,
+            (None, Some(_)) => std::cmp::Ordering::Greater,
+            (None, None) => a.name.cmp(&b.name),
+        },
+    );
+
+    txn.commit()
+        .await
+        .map_err(|e| format!("Failed to commit transaction: {}", e))?;
+
+    Ok(UIGroups {
+        groups: groups_info.into(),
+    })
 }
-async fn get_one_group(db: impl ConnectionTrait, id: GroupId) -> OneGroup {
-    todo!()
+
+async fn get_one_group(db: &impl ConnectionTrait, id: GroupId) -> Result<OneGroup, String> {
+    let group = groups::Entity::find_by_id(id.0)
+        .one(db)
+        .await
+        .map_err(|e| format!("Failed to find group: {}", e))?
+        .ok_or_else(|| format!("Group not found: {:?}", id.0))?;
+
+    let last_message = messages::Entity::find()
+        .filter(messages::Column::GroupUuid.eq(id.0))
+        .order_by_desc(messages::Column::CreateAt)
+        .one(db)
+        .await
+        .map_err(|e| format!("Failed to find messages: {}", e))?;
+
+    let (last_msg, last_msg_time) = match last_message {
+        Some(msg) => (Some(msg.content), Some(msg.create_at)),
+        None => (None, None),
+    };
+
+    Ok(OneGroup {
+        id,
+        name: group.group_name,
+        last_msg,
+        last_msg_time,
+    })
 }
+
